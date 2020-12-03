eaf5174ae6d000c1a06b2693b25e12315afba3e4
shahrishabh7 <75043888+shahrishabh7@users.noreply.github.com>
2020-12-02 21:00:54 -0600

    Update and rename convex hull.py to ConvexHull.py

--- ConvexHull.py
+++ ConvexHull.py
@@ -1,22 +1,13 @@
 #  File: Hull.py
 
-#  Description:
+#  Description: A convex hull is the smallest convex polygon that will enclose a set of points. In a convex polygon a line joining any two
+#  points in the polygon will lie completely within the polygon. One way to visualize a convex hull is as follows: imagine there are nails
+#  sticking out over the distribution of points. Place a rubber band such that it encircles all the nails. The figure described by the rubber
+#  band is a convex hull.
+
+#  This program will read in a set of points and accordingly print the vertices of the convex hull starting at the extreme left point and going clockwise
 
 #  Student Name:Rishabh Shah
-
-#  Student UT EID: rks2387
-
-#  Partner Name: Harshith Vanam
-
-#  Partner UT EID: hkv232
-
-#  Course Name: CS 313E
-
-#  Unique Number: 50845
-
-#  Date Created: 10/29/2020
-
-#  Date Last Modified: 10/29/2020
 
 import math
 
@@ -82,10 +73,7 @@ class Point (object):
 # Input: p, q, r are Point objects
 # Output: compute the determinant and return the value
 def det (p, q, r):
-    #det = (p.x*q.y + q.x*r.y + r.x*p.y) - (p.y*q.x + q.y*r.x + r.y*p.x)
-
-    det = (p.x * (q.y - r.y)) + (q.x * (r.y - p.y)) + (r.x * (p.y - q.y))
-    return det
+    return (p.x * (q.y - r.y)) + (q.x * (r.y - p.y)) + (r.x * (p.y - q.y))
 
 # Input: sorted_points is a sorted list of Point objects
 # Output: computes the convex hull of a sorted list of Point objects
@@ -95,41 +83,29 @@ def det (p, q, r):
 def convex_hull (sorted_points):
     points = sorted_points
     data_size = len(sorted_points)
+    
     upper = []
-
     upper.append(points[0])
     upper.append(points[1])
-
     for i in range(2,data_size):
         upper.append(points[i])
-
         while (len(upper) >= 3) and (det(upper[-3], upper[-2], upper[-1]) >= 0):
             upper.pop(-2)
 
-
-
     lower = []
-
     lower.append(points[data_size-1])
     lower.append(points[data_size-2])
-
     for i in range(data_size-3, -1, -1):
         lower.append(points[i])
-
         while (len(lower) >= 3) and (det(lower[-3], lower[-2], lower[-1]) >= 0):
             lower.pop(-2)
-
-
     lower.pop(0)
     lower.pop(-1)
 
-
     convex_hull = []
-
 
     for x in upper:
         convex_hull.append(x)
-
 
     for x in lower:
         convex_hull.append(x)
@@ -146,10 +122,8 @@ def area_poly (convex_poly):
 
     pos_x = []
     pos_y = []
-
     neg_x = []
     neg_y = []
-
 
     for x in range(data_size):
         pos_x.append(convex_poly[x].x)
@@ -176,18 +150,8 @@ def area_poly (convex_poly):
         neg_sum -= sum
 
     total = pos_sum + neg_sum
-
     total = math.fabs(total) / 2
-
     return total
-
-
-# Input: no input
-# Output: a string denoting all test cases have passed
-def test_cases():
-    # write your own test cases
-
-    return "all test cases passed"
 
 def main():
     # create an empty list of Point objects
@@ -195,9 +159,7 @@ def main():
 
     # read data from standard input
     data = open("hull.in", "r")
-
     data_size = int(data.readline())
-
     coord = []
 
     # read line by line, create Point objects and store in a list
@@ -217,15 +179,11 @@ def main():
         else:
             counter += 1
 
-
     # get the convex hull
     chull = convex_hull(points)
-
-    # run your test cases
-
+    
     # print your results to standard output
     print("Convex Hull")
-
 
     # print the convex hull
     for x in chull:
